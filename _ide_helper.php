9265429fe9381b5fc7fe8f2edeebeebce835d734
huangkd <huangkd2008@163.com>
2018-06-11 02:15:24 +0000

    composer require fzaninotto/faker=1.4

--- _ide_helper.php
+++ _ide_helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.5.40 on 2018-05-31 03:09:18.
+ * Generated for Laravel 5.5.40 on 2018-06-11 02:07:06.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
