91d17bb240b56b18d83149c23633bf57ee6b8129
huangkd <huangkd2008@163.com>
2018-06-11 03:27:21 +0000

    composer require fzaninotto/faker=1.4

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -6,7 +6,7 @@
             <div class="col-md-12">
                 <div class="col-md-offset-2 col-md-8">
                     <section class="user_info">
-                        @include('shared.﻿_user_info', ['user' => $user])
+                        @include('shared._user_info', ['user' => $user])
                     </section>
                 </div>
             </div>
